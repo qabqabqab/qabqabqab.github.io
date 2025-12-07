d42ef135692f5807675715fc620be2b1c35de4fe
qabqabqab <qab@bananagroove.com>
2025-12-07 18:41:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,37 @@
+const minhasArtes = [
+  {
+    titulo: "a cura que eu preciso",
+    categoria: "paintings",
+    info: "gouache on canvas, 2021",
+    arquivo: "imagens/arte1.jpg", 
+    extra: "posted on Foundation",
+    // AQUI ESTÁ A NOVIDADE: O LINK
+    linkExterno: "https://foundation.app/mint/base/0x0293969e41EE7AB4aad619D47780b2bFCFCF9570/1"
+  },
+  {
+    titulo: "heart-shaped face",
+    categoria: "paintings",
+    info: "gouache on canvas, 2023",
+    arquivo: "imagens/arte2.jpg",
+    extra: "posted on Zora",
+    // Outro exemplo de link
+    linkExterno: "https://zora.co/coin/base:0xffe23b71c814d8d7a85a76f507b5ccf493972616?referrer=0x9904ceddba825a18bdf7e1984a886d23394ccbcc" 
+  },
+  {
+    titulo: "eu quero ser maior que essas muralhas que eles construiram ao meu redor",
+    categoria: "paintings",
+    info: "gouache on paper, 2022",
+    arquivo: "imagens/arte3.jpg",
+    extra: "posted on Exchange", // Se não tiver nada escrito
+    linkExterno: "https://exchange.art/single/2B3Wps1bHBVhaidcTpapjckMX9fqYeLpwoLSRksVBe4D" // Deixe vazio se não tiver link
+  },
+  {
+    titulo: "tumulto",
+    categoria: "paintings",
+    info: "gouache, china ink, crayon and acrylic pen on canvas, 2022",
+    arquivo: "imagens/arte4.jpg",
+    extra: "posted on Exchange",
+    linkExterno: "https://exchange.art/single/95UudAr8amSVD9WMDCnkptxWacpmbaPbSAd8YLpjn5QC"
+  },
+
+];
