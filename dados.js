d5d9bb620bb6e552bf3c7b88d0c99a390d4f510f
qabqabqab <qab@bananagroove.com>
2025-12-08 01:43:36 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -5,6 +5,7 @@ const minhasArtes = [
     info: "gouache on canvas, 2021",
     arquivo: "imagens/arte1.jpg", 
     extra: "posted on Foundation",
+    tipo: "imagem",
     // AQUI ESTÁ A NOVIDADE: O LINK
     linkExterno: "https://foundation.app/mint/base/0x0293969e41EE7AB4aad619D47780b2bFCFCF9570/1"
   },
@@ -14,6 +15,7 @@ const minhasArtes = [
     info: "gouache on canvas, 2023",
     arquivo: "imagens/arte2.jpg",
     extra: "posted on Zora",
+    tipo: "imagem",
     // Outro exemplo de link
     linkExterno: "https://zora.co/coin/base:0xffe23b71c814d8d7a85a76f507b5ccf493972616?referrer=0x9904ceddba825a18bdf7e1984a886d23394ccbcc" 
   },
@@ -22,7 +24,8 @@ const minhasArtes = [
     categoria: "paintings",
     info: "gouache on paper, 2022",
     arquivo: "imagens/arte3.jpg",
-    extra: "posted on Exchange", // Se não tiver nada escrito
+    extra: "posted on Exchange",
+    tipo: "imagem",
     linkExterno: "https://exchange.art/single/2B3Wps1bHBVhaidcTpapjckMX9fqYeLpwoLSRksVBe4D" // Deixe vazio se não tiver link
   },
   {
@@ -31,7 +34,17 @@ const minhasArtes = [
     info: "gouache, china ink, crayon and acrylic pen on canvas, 2022",
     arquivo: "imagens/arte4.jpg",
     extra: "posted on Exchange",
+    tipo: "imagem",
     linkExterno: "https://exchange.art/single/95UudAr8amSVD9WMDCnkptxWacpmbaPbSAd8YLpjn5QC"
   },
+  {
+  titulo: "tijolin",
+    categoria: "3d",
+    info: "ink on brick, 2021",
+    arquivo: "imagens/tijolin.glb",
+    extra: "posted on Zora",
+    tipo: "3d",
+    linkExterno: "https://opensea.io/item/zora/0x281a77222a49b30acecc907cdc97f0109345784b/"
+},
 
 ];
